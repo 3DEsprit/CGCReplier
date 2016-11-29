b4cea8c438b5b4674ad7045076b6992b35d9337c
DBrown <vilesyn@gmail.com>
2016-11-28 22:30:04 -0600

    base layout of extension

---
+++ App/process.js
@@ -0,0 +1,35 @@
+// Stuff
+
+// Send Results to Pop Up
+function render(status) {
+  document.getElementById('status').textContent = status;
+}
+
+// Find Questions without Replies
+function getReplyCount(course, callback, errorCallback) {
+  var mainUrl = 'https://cgcookie.com/';
+  var link = new XMLHttpRequest();
+  link.open('GET', mainUrl + course);
+  link.responseType = 'json';
+  link.onload = function() {
+    var res = link.response;
+    if(!res || !res.responseData) {
+      errorCallback('No response back! No bueno!');
+      return;
+    }
+    var result = res;
+    render(result);
+    callback();
+  };
+  link.onerror = function() {
+    errorCallback('Network error.');
+  };
+  link.send();
+}
+
+// Listen for change
+document.addEventListener('DOMContentLoaded', function() {
+
+  var courses = ['flow/modeling-in-blender/'];
+  getReplyCount(courses[0]);
+});
