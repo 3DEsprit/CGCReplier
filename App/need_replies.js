d21a23b5e5d256dd3d0fc51bfa2aed16776d57df
Dennis Brown <vilesyn@gmail.com>
2016-12-06 01:49:55 -0600

    merge update fix

---
+++ App/need_replies.js
@@ -0,0 +1,21 @@
+(function() {
+  console.log('need loaded');
+  // add replies to list object
+  window.replyCheck = window.replyCheck || {};
+
+  replyCheck.NeedReplies = function() {
+    this._replies = {};
+  };
+
+  replyCheck.NeedReplies.prototype = {
+    setReply: function(reply) {
+      console.log('set');
+    },
+    removeReply: function(reply) {
+      console.log('remove');
+    },
+    findReply: function(reply) {
+      console.log('find');
+    }
+  };
+})();
---
+++ App/popup.js
@@ -0,0 +1,34 @@
+// Stuff
+(function() {
+  // list replies and add in list. Grab titles
+  console.log('popup loaded');
+
+  var openReplies = new replyCheck.OpenReplies._replies;
+
+  var table = document.querySelector('#results');
+
+  var open = new replyCheck.OpenReplies;
+
+  function testStuff() {
+    console.log('Replies: ', open._replies);
+  }
+
+  function createQuestionLink(q) {
+    var div = document.createElement('div');
+    div.className = 'questions';
+    var a = document.createElement('a');
+    a.className = 'question';
+    a.target = '_blank';
+    a.src = q.url;
+    div.appendChild(a);
+
+    return div;
+  }
+
+  function start() {
+    console.log('start popup');
+    testStuff();
+  }
+
+  start();
+})();
