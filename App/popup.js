9296617200e8a18883f4e558cf3a5ca2f2c48db1
DBrown <vilesyn@gmail.com>
2016-12-10 02:10:32 -0600

    change popup link addition

--- App/popup.js
+++ App/popup.js
@@ -5,29 +5,33 @@
 
   // call object from background here
   // var open = replyCheck.OpenReplies;
-  var need = new replyCheck.NeedReplies;
   var div = document.querySelector('#results');
+  var re = /(?:https:\/\/cgcookie.com\/course\/|lesson\/)([a-z\-]*)/i;
 
   function createQuestionLink(url) {
+    var match = re.exec(url);
+    var title = match[1];
     var link = document.createElement('div');
     link.className = 'questions';
     var a = document.createElement('a');
     a.className = 'question';
     a.target = '_blank';
-    a.src = need.mainUrl + url + '#discussion';
-    a.innerHTML = url;
+    a.href = replyCheck.getNeedReplies().mainUrl + url + '#discussion';
+    a.innerHTML = title;
     div.appendChild(a);
+    div.appendChild('<br>');
   }
 
   // calling array from Object and output to console
   function searchList() {
-    console.log(replyCheck.OpenReplies._replies);
-    open._replies.map((out) => {
+    console.log('search');
+    replyCheck.getNeedReplies()._replyList.map((out) => {
       createQuestionLink(out);
     });
+    // console.log(replyCheck.getNeedReplies().forEach());
   }
 
-  chrome.runtime.onStartup.addListener(() => {
+  chrome.runtime.onStartup.addListener((out) => {
     // Listen for Background to update badge
     console.log('Popup Start', out);
   });
