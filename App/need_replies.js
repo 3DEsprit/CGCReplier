bb0aabcb7daea34d6db06c6dfb8975f6e26758fe
DBrown <vilesyn@gmail.com>
2016-12-10 02:08:08 -0600

    setup singleton object

--- App/need_replies.js
+++ App/need_replies.js
@@ -2,13 +2,12 @@
   // add replies to list object
   window.replyCheck = window.replyCheck || {};
   var mainUrl = 'https://cgcookie.com/';
-  var match;
-  var open = new replyCheck.OpenReplies;
   var utils = new replyCheck.Utils;
   var re = /(?:discussion--item__parent)*(?:<span>)*(?:discussion--reply-count">)(\d{1})/ig;
 
   replyCheck.NeedReplies = function() {
     this._total = 0;
+    this._replyList = [];
     this.mainUrl = 'https://cgcookie.com/';
     this.courses = [
       'course/fundamentals-of-rigging/',
@@ -19,17 +18,15 @@
   };
 
   replyCheck.NeedReplies.prototype = {
-    checkList: function(cb) {
-      open._replies = [];
+    checkList: function(arr, cb) {
       for(var url of this.courses) {
-        var fullUrl = this.mainUrl + url + '#discussion';
+        var fullUrl = mainUrl + url + '?discussion-page=1#discussion';
         utils.fetchPage(fullUrl, function(out) {
           var match = out.match(re);
           for(var r in match) {
             if(match[r].slice(-1, match[r].length) === '0')
-              open._replies.push(url);
-              console.log(open._replies);
-              return;
+              console.log(arr);
+              return arr.push(fullUrl);
           }
         });
       }
@@ -44,6 +41,19 @@
     findLesson: function(url) {
       console.log('lessons');
       // li lesson-list-item > child a
+    },
+    forEach: function(cb) {
+      for(var key of replyCheck.getNeedReplies()._replyList)
+        cb(key);
     }
+  };
+
+  replyCheck.getNeedReplies = function() {
+    var background = chrome.extension.getBackgroundPage();
+    console.log('background: ', background);
+    if (!background.replyCheck.hasOwnProperty("needReplies"))
+      background.replyCheck.needReplies = new replyCheck.NeedReplies;
+
+    return background.replyCheck.needReplies;
   };
 })();
