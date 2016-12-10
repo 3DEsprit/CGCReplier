7a1965c492d032302acc417fe0ee33232b3d5be5
DBrown <vilesyn@gmail.com>
2016-12-10 02:08:46 -0600

    fix to populate instance array

--- App/bg.js
+++ App/bg.js
@@ -1,18 +1,21 @@
 // background process
 (function() {
   // watch intervals and start searching
-  var searchState = 0, lastTime = 0;
-  var utils = new replyCheck.Utils;
-  var need = new replyCheck.NeedReplies;
-  var prefs = new replyCheck.Prefs;
-  var pollTime = prefs._pollTime;
-  var waitTime = prefs._waitTime;
+  // var searchState = 0, lastTime = 0;
+  // var utils = new replyCheck.Utils;
+  // var prefs = new replyCheck.Prefs;
+  // var open = new replyCheck.OpenReplies;
+  // var pollTime = prefs._pollTime;
+  // var waitTime = prefs._waitTime;
 
-  var results = document.querySelector('results');
+  // var results = document.querySelector('results');
 
   function initialCheck() {
-    need.checkList((out) => {
-      chrome.runtime.sendMessage({action: 'done'});
+    replyCheck.getNeedReplies().checkList(replyCheck.getNeedReplies()._replyList, () => {
+      replyCheck.getNeedReplies().forEach((out) => {
+        console.log(out);
+        chrome.runtime.sendMessage({action: 'done'});
+      });
     });
   }
 
