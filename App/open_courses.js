a36131066230cf8f1b04ee864dd4250e2cc6306d
DBrown <vilesyn@gmail.com>
2016-12-11 01:11:57 -0600

    move courses to own file, and rename

---
+++ App/open_courses.js
@@ -0,0 +1,34 @@
+(function() {
+  // background script
+  // reply list getter/setter and list
+  window.replyCheck = window.replyCheck || {};
+
+  // Adding methods and array to object
+  replyCheck.Courses = function() {
+    this.courseList = [
+      'course/blender-basics/', 'course/mesh-modeling-fundamentals/',
+      'course/introduction-to-texturing/', 'course/shading-fundamentals-in-cycles/',
+      'course/fundamentals-of-rigging/', 'course/fundamentals-of-animation/',
+      'course/fundamentals-of-dynamics/', 'course/fundamentals-of-lighting/',
+      'course/introduction-to-rendering/', 'course/fundamentals-of-compositing/',
+      'course/modeling-with-modifiers/', 'course/fundamentals-of-digital-sculpting/',
+      'course/introduction-to-retopology/'
+    ]
+  }
+
+  replyCheck.Courses.prototype = {
+    forEach: function(cb) {
+      // for (var reply of need._replyList)
+        cb();
+    },
+    setCourse: function(replies) {
+      // set
+    },
+    removeCourse: function(reply) {
+      // remove
+    },
+    findCourse: function(reply) {
+      // find?
+    }
+  };
+})();
