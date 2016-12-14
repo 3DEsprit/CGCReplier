33ce1259ff5dba4f53526635f622edd6e59758c9
DBrown <vilesyn@gmail.com>
2016-12-14 16:47:35 -0600

    finish added flow courses

--- App/open_courses.js
+++ App/open_courses.js
@@ -12,11 +12,34 @@
         'course/fundamentals-of-dynamics/', 'course/fundamentals-of-lighting/',
         'course/introduction-to-rendering/', 'course/fundamentals-of-compositing/',
         'course/modeling-with-modifiers/', 'course/fundamentals-of-digital-sculpting/',
-        'course/introduction-to-retopology/'
+        'course/introduction-to-retopology/', 'course/modeling-video-game-assets-in-blender/',
+        'course/texturing-game-assets-with-blender/'
     ];
-    this.Concept = [];
-    this.Sculpt = [];
-    this.Unity = [];
+    this.Concept = [
+      'course/digital-painting-basics/', 'course/lighting-and-values/',
+      'course/lighting-and-values-2/', 'course/color-course-understanding-color/',
+      'course/introduction-to-materials-and-texture/', 'course/the-perspective-course/',
+      'course/tools-of-the-trade/', 'course/important-drawing-tips/',
+      'course/complete-drawing-timelapses-and-inspiration-lectures/',
+      'course/creating-the-game-asset-concept-art/'
+    ];
+    this.Sculpt = [
+      'course/sculpting-basics/', 'course/fundamentals-sculpting-with-clay/',
+      'course/introduction-to-human-anatomy-and-basic-proportions/',
+      'course/introduction-to-mold-making-and-molding-techniques/',
+      'course/introduction-to-casting-casting-techniques/'
+    ];
+    this.Unity = [
+      'course/unity-basics-introduction-for-beginners/', 'course/fundamentals-of-the-unity-interface/',
+      'course/fundamentals-of-materials-and-textures/', 'course/fundamentals-of-lighting-2/',
+      'course/fundamentals-of-physics/', 'course/fundamentals-of-ui-design/',
+      'course/fundamentals-of-2d-development/', 'course/fundamentals-of-particle-systems/',
+      'course/fundamentals-of-terrain-generation/', 'course/fundamentals-of-tree-creation/',
+      'course/fundamentals-of-pathfinding/', 'course/fundamentals-of-image-effects/',
+      'course/fundamentals-of-mecanim-animation/', 'course/fundamentals-of-rigged-animations/',
+      'course/fundamentals-of-game-publishing/', 'course/fundamentals-of-weapon-mechanics/',
+      'course/fundamentals-of-fps-ui-design/', 'course/using-game-assets-in-unity/'
+    ];
     this.BlenderLesson = [];
     this.ConceptLesson = [];
     this.SculptLesson = [];
