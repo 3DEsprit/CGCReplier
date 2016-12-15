4ea77f097709d10269ada2102a404c6403c48be7
Dennis Brown <vilesyn@gmail.com>
2016-12-15 11:14:17 -0600

    add messaging and remove badge double

--- CGCReplier/popup.js
+++ CGCReplier/popup.js
@@ -6,11 +6,13 @@
   // call object from background here
   var needFirst = replyCheck.getNeedReplies();
   var re = /(?:https:\/\/cgcookie.com\/course\/|lesson\/)([a-z\-]*)/i;
-  var questions = 0;
+  var questions = 0, topics = 0;
 
   function createQuestionLink(url, flow) {
     var div = document.querySelector('.' + flow);
+    var header = document.querySelector('.result');
     if(div.style.display == '') { div.style.display = 'block'; }
+    if(header.style.display !== '') { div.style.display = 'none'; }
     var match = re.exec(url);
     var title = match[1].toUpperCase().replace(/\-/gi, ' ');
     var link = document.createElement('div');
@@ -21,7 +23,6 @@
     a.href = replyCheck.getNeedReplies().mainUrl + url;
     a.innerHTML = title;
     div.appendChild(a);
-    needFirst._total += 1;
   }
 
   function searchList(flow, cb) {
@@ -30,18 +31,34 @@
         createQuestionLink(n, flow);
         questions++;
         if(questions == needFirst._questionList[flow].length) { cb('done'); }
-        // needFirst._questionList = [];
       }
-    } else { cb('done'); }
+    } else {
+      topics++;
+      cb('done');
+    }
+  }
+
+  function createMessage(text) {
+    var div = document.querySelector('.result');
+    if(div.style.display == '') { div.style.display = 'block'; }
+    var message = document.createElement('h3');
+    message.innerHTML = text;
+    div.appendChild(message);
   }
 
   function start() {
-    // console.log('Starting Popup ' + chrome.app.getDetails().version);
+    console.log('Starting Popup ' + chrome.app.getDetails().version);
+    topics = 0;
     searchList('Blender', (out) => {
       if(out === 'done') { searchList('Concept', (out) => {
         if(out === 'done') { searchList('Sculpt', (out) => {
           if(out === 'done') { searchList('Unity', (out) => {
-            if(out === 'done') { console.log('Done traversing lists'); }
+            if(out === 'done') {
+              console.log('Done traversing lists');
+              if(topics === 4) {
+                createMessage('no questions');
+              }
+            }
           })}
         })}
       })}
